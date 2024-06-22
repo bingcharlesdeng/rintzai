6ae4e1338409eab84a9e668c8f6798f5394b0c07
bingcharlesdeng <bingcharlesdeng@gmail.com>
2024-06-21 22:11:01 -0700

    wtf

--- src/components/Home/FeatureSection.js
+++ src/components/Home/FeatureSection.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaUser, FaClipboard, FaChartBar, FaBook, FaComments, FaQuoteLeft, FaHeart, FaImages } from 'react-icons/fa';
-import './featureSection.css';
 
 
 const features = [
