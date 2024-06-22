b070219b45cb3e500c805a1d79a730aedb5cafab
bingcharlesdeng <bingcharlesdeng@gmail.com>
2024-06-21 21:43:22 -0700

    Feature section breaking

--- src/components/Home/FeatureSection.js
+++ src/components/Home/FeatureSection.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaUser, FaClipboard, FaChartBar, FaBook, FaComments, FaQuoteLeft, FaHeart, FaImages } from 'react-icons/fa';
-import "./featureSection.css";
+import './featureSection.css';
 
 const features = [
   { name: 'Profile', icon: FaUser, path: '/profile' },
