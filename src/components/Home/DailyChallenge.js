2b33311531d592b86d96e518c17b32ab26b2eece
bingcharlesdeng <bingcharlesdeng@gmail.com>
2024-06-21 23:25:20 -0700

    bruh

--- src/components/Home/DailyChallenge.js
+++ src/components/Home/DailyChallenge.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './DailyChallenge.css';
+import './dailyChallenge.css';
 
 const DailyChallenge = () => {
   const [isCompleted, setIsCompleted] = useState(false);
--- src/components/Home/StreakCounter.js
+++ src/components/Home/StreakCounter.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './StreakCounter.css';
+import './streakCounter.css';
 
 const StreakCounter = () => {
   const [streak, setStreak] = useState(0);
