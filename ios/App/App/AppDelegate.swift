2978a303b40e836cb2c93d7ae812046eeaa7e0d4
bingcharlesdeng <bingcharlesdeng@gmail.com>
2024-06-30 22:45:24 -0700

    hello

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,17 +1,31 @@
+import UIKit
 import Capacitor
-import CapacitorCordova
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+
     var window: UIWindow?
-    var bridgeViewController: CAPBridgeViewController?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
         print("Application is launching")
         
-        // Enable Capacitor logging
-        CapacitorBridge.enableLogging(true)
+        // Capacitor logging is handled automatically in newer versions
+        // If you need custom logging, you can add it here
         
         return true
+    }
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        // Called when the app was launched with a url. Feel free to add additional processing here,
+        // but if you want the App API to support tracking app url opens, make sure to keep this call
+        return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
+    }
+
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        // Called when the app was launched with an activity, including Universal Links.
+        // Feel free to add additional processing here, but if you want the App API to support
+        // tracking app url opens, make sure to keep this call
+        return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
     }
 }
